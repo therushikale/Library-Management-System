7e21fc02c126df804047d6cb9b2297f324cfb390
therushikale <rushikale136@gmail.com>
2022-12-05 08:55:06 +0530

    new chnages

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+  slidesToShow: 3,
+  dots:true,
+  centerMode: true,
+  });
+});
+function showabout(){
+  window.open("https://codepen.io/shreyagour/full/qBmVvyK");
+}
